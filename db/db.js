9b334c8895ce4e7892e073cbb8714fc731875bba
Princess Onyiri <princessonyiri@gmail.com>
2021-02-27 14:28:23 -0500

    again

--- db/db.js
+++ db/db.js
@@ -4,6 +4,9 @@ const Sequelize= require('sequelize');
 const db= new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/southpark',
 {
     logging: false,
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
 
 module.exports= db;
